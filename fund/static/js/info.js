951cc19ef8648001c5795cf45629d3d1d73b0bac
lekhan113 <khanpro112@gmail.com>
2022-12-03 17:25:51 +0700

    add conect and polices

--- fund/static/js/info.js
+++ fund/static/js/info.js
@@ -7,3 +7,20 @@ document.getElementById("close_popup").onclick = function(){
 	document.getElementById("popup").style.opacity = "0";
 	document.getElementById("popup").style.pointerEvents = "none";
 }
+
+document.getElementById("polices").onclick = function(){
+	document.getElementById("frame_polices").style.opacity = "100%";
+	document.getElementById("frame_polices").style.pointerEvents = "all";
+}
+
+document.getElementById("close_polices").onclick = function(){
+	document.getElementById("frame_polices").style.opacity = "0";
+	document.getElementById("frame_polices").style.pointerEvents = "none";
+}
+
+document.getElementById("facebook_block").onclick = function(){
+	alert("Comming soon");
+}
+document.getElementById("google_block").onclick = function(){
+	alert("Comming soon");
+}
